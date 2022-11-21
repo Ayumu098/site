c04abe8f7e421cc343b3602241b24331c50d3445
Ayumu098 <stephen.singer.098@gmail.com>
2022-11-21 17:26:48 +0800

    Add core packages and remove unused files

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Work in Progress</p>
+	  <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
