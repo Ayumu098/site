b9f89f557df7a34268bc6ac69bc7c49d00d79f14
Ayumu098 <stephen.singer.098@gmail.com>
2022-11-21 17:39:38 +0800

    Apply code formatting to entire workspace

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <p>Work in Progress</p>
-	  <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
     </div>
   );
 }
